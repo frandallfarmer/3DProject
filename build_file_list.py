13ba58f4c7d8885b13f4e48e58a0f4da86c957bb
randy <randy.farmer@pobox.com>
2026-01-14 16:47:53 -0800

    testing workflows

--- build_file_list.py
+++ build_file_list.py
@@ -2,6 +2,7 @@
 """
 Build script to generate a JSON file listing all STL files in built_stls directory.
 Run this script whenever STL files are added/removed/changed.
+(This comment exists to test workflow triggers)
 """
 
 import json
